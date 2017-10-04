d83df7caa8d51d879227871ee62785549a3fc2a3
imcvampire <quocanh96@gmail.com>
2017-10-04 23:08:05 +0700

    feat(client): add Router and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
+
 const App = () => (
-  <div>Hello!</div>
+  <BrowserRouter>
+    <div>
+      <Navbar />
+
+      <Switch>
+        <Route
+          exact
+          path="/create"
+        />
+        <Route
+          exact
+          path="/answer"
+        />
+
+        <Redirect to="/create" />
+      </Switch>
+    </div>
+  </BrowserRouter>
+
 )
 
 export default App
