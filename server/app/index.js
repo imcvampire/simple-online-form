3b9a469b68be2e240fcf8f8f57a9e3a8a6ffc927
imcvampire <quocanh96@gmail.com>
2017-10-07 13:42:15 +0700

    feat(server): finish api

---
+++ server/app/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const Question = require('./model/Question.js')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/api/questions', (req, res) = {
+  Question.getAllQuestion((error, questions) => {
+    if (error) res.status(500).end()
+
+    res.json(questions)
+  })
+})
+
+app.post('/api/questions', (req, res) => {
+  Question.addQuestion(req.body, (error) => {
+    if (error) res.status(500).end()
+
+    res.status(204).end()
+  })
+})
+
+module.exports = app
